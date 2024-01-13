99caad13bb25a9d43a7a18ad3d08dab639bd102e
s87298 <s87298@bht-berlin.de>
2024-01-13 14:56:58 +0100

    glb to ply converter

---
+++ scripts/glb_to_ply.py
@@ -0,0 +1,19 @@
+import open3d as o3d
+import sys
+
+def glb_to_pointcloud(glb_path, n):
+    mesh = o3d.io.read_triangle_mesh(glb_path)
+    pcd = mesh.sample_points_uniformly(number_of_points=n)
+
+    return pcd
+
+glb_file_path = sys.argv[1]
+
+if(len(sys.argv) <= 2):
+    n = 10000
+else:
+    n = int(sys.argv[2])
+pointcloud = glb_to_pointcloud(glb_file_path, n)
+
+o3d.visualization.draw_geometries([pointcloud])
+o3d.io.write_point_cloud("output.ply", pointcloud)
